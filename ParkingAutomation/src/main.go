abe12c488eb734ea6bb66b45e953e1e83e237895
mertovich <88425310+mertovich@users.noreply.github.com>
2022-03-19 15:17:39 +0300

    add(Menu): Exit option

--- ParkingAutomation/src/main.go
+++ ParkingAutomation/src/main.go
@@ -2,32 +2,48 @@ package main
 
 import (
 	"fmt"
+	"parking/src/car"
+	"parking/src/errors"
 )
 
 func main() {
 	fmt.Println(`--- Start ---`)
 
-	L:
+L:
 	for true {
 		var choice string
 		// menu
 		fmt.Println(`[1] Register`)
 		fmt.Println(`[2] Search`)
 		fmt.Println(`[3] All Records`)
+		fmt.Println(`[4] Exit`)
+		fmt.Print(`Choice: `)
 		fmt.Scan(&choice)
 
 		// menu func
 		switch choice {
 		case `1`:
 			fmt.Println(`Case 1`)
-			break L
+			// read value
+			numberPlate := ``
+			carLocation := ``
+			fmt.Print(`Number plate: `)
+			fmt.Scan(&numberPlate)
+			fmt.Print(`Car location: `)
+			fmt.Scan(&carLocation)
+			c := car.Car{numberPlate, carLocation}
+			car.Add(c)
 		case `2`:
 			fmt.Println(`Case 2`)
 			break L
 		case `3`:
 			fmt.Println(`Case 3`)
+			car.GetAllList()
+		case `4`:
+			fmt.Println(`Exit`)
 			break L
 		default:
+			errors.IncorrectEntry()
 			break L
 		}
 
