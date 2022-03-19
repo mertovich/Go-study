9a58053a57e80c9cbb95ae62a7c127f374fa2a8b
mertovich <88425310+mertovich@users.noreply.github.com>
2022-03-19 15:07:39 +0300

    add(Car): Add & GetAllList func

--- ParkingAutomation/src/car/car.go
+++ ParkingAutomation/src/car/car.go
@@ -1,7 +1,21 @@
 package car
 
+import "fmt"
+
 type Car struct {
 	Number_plate string
 	Car_location string
 }
 
+// Global variable
+var CarList []Car
+
+func Add(c Car){
+	CarList = append(CarList,c)
+}
+
+func GetAllList() {
+	for _, car := range CarList {
+		fmt.Println(`Plate: `,car.Number_plate,`Location: `,car.Car_location)
+	}
+}
